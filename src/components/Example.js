994b159300739948a9f21845e26868316c267e6c
liushunan <cowkeys@outlook.com>
2018-08-30 19:59:00 +0800

    update

--- src/components/Example.js
+++ src/components/Example.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { Tabs } from 'antd';
 import {connect} from "dva/index";
 import MainPanel from './MainPanel';
+import Common from './Common';
 import GameContract from './GameContract';
 import TokenContract from './TokenContract';
 
@@ -16,13 +17,16 @@ const Example = () => {
   return (
     <div>
     <Tabs defaultActiveKey="1" onChange={callback}>
-      <TabPane tab="main" key="1">
+      <TabPane tab="统一接口" key="1">
+      <Common/>
+      </TabPane>
+      <TabPane tab="代币购买道具用例" key="3">
         <MainPanel/>
       </TabPane>
-      <TabPane tab="游戏测试合约" key="2" style={{ textAlign: 'left'}}>
+      <TabPane tab="游戏测试合约" key="4" style={{ textAlign: 'left'}}>
         <GameContract/>
       </TabPane>
-      <TabPane tab="代币测试合约" key="3" style={{ textAlign: 'left'}}>
+      <TabPane tab="代币测试合约" key="5" style={{ textAlign: 'left'}}>
         <TokenContract/>
       </TabPane>
     </Tabs>
